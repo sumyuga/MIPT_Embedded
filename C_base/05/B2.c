292a1b0ed5526926c1d8a8fc21d13057bc186e79
sumyuga <sumyuga@gmail.com>
2025-05-20 13:21:20 +0300

    Initial commit

---
+++ C_base/05/B2.c
@@ -0,0 +1,22 @@
+/*
+B1 – Квадраты от А до В:
+Ввести два целых числа a и b (a ≤ b) и вывести квадраты всех чисел от a до b
+Input: Два целых числа по модулю не больше 100
+Output: Квадраты чисел от a до b
+*/
+#include <stdio.h>
+
+int main(void)
+{
+    int num1, num2, square;
+    scanf("%d%d", &num1, &num2);
+    if (num1 > num2 || num2 > 100) return 0;
+    int i = num1;
+    while (i<=num2) {
+        square = i*i;
+        i++;
+        printf("%d %s", square, i>num2?"\n":"");
+    }
+    // puts("");
+    return 0;
+}
